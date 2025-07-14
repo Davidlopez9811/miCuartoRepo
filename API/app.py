e002bd09bd5e277efe18ee686deac83a9791d0bf
DAVID LOPEZ <lopezsandovaldavidmauricio@gmail.com>
2025-07-14 20:38:25 +0200

    Metodo de platzi agregado

--- API/app.py
+++ API/app.py
@@ -14,3 +14,9 @@ def get_familia():
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
     return rows
+
+
+@app.get("/cursosPlatzi")
+def get_Platzi():
+    rows = ["Docker", "Bash", "Linux", "Ingles", "python", "Javascript", "Azure"]
+    return rows
